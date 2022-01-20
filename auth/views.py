7ced4d357f4ed7b3247697c3c788ad45bda9933f
suraj kumar <67819248+surajkumarbhagat71@users.noreply.github.com>
2022-01-20 14:07:38 +0530

    Update views.py

--- auth/views.py
+++ auth/views.py
@@ -64,4 +64,74 @@ class Logout(APIView):
         data = RefreshToken(token)
         data.blacklist()
         return Response("Successful Logout", status=status.HTTP_200_OK)
+    
+    from django.shortcuts import render
+
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
+
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import status
+
+from .serializers import *
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+from django.contrib.auth.hashers import (
+    check_password, is_password_usable, make_password,
+)
+
+class GetLoginToken(APIView):
+    def post(self, request):
+        user = User.objects.filter(username=request.data.get("username")).first()
+        password = request.data.get("password")
+        role = User.objects.get(username=request.data.get('username'))
+        data = role.role
+
+        if user is not None and user.check_password(password):
+
+            refresh = RefreshToken.for_user(user)
+            context = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'username':str(role.username),
+                'role': str(data),
+            }
+            return Response(context, status.HTTP_200_OK)
+
+            # if user.is_active:
+            #     refresh = RefreshToken.for_user(user)
+            #     context = {
+            #         'refresh': str(refresh),
+            #         'access': str(refresh.access_token),
+            #     }
+            #     return Response(context, status.HTTP_200_OK)
+            # else:
+            #     context = {
+            #         "message": "Please verify your email first!"
+            #     }
+            #     return Response(context, status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+        else:
+            context = {
+                "detail": "No active account found with the given credentials"
+            }
+            return Response(context, status.HTTP_401_UNAUTHORIZED)
+
+
 
